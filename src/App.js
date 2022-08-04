388ef4de07118abfd431d962d237c1fdc39a2ecc
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-08-04 10:37:40 +0530

    :fire: succcessfully created the modal opening and closing function using reactstrap modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ToDoList from './components/ToDoList/ToDoList';
+// due to the below line bootstrap is available to all the components in the app
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-
+      <ToDoList />
     </div>
   );
 }
--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import 
+import React, { useState } from "react";
+import "./ToDoList.css";
+import CreateTask from "../Modals/createTask";
 const ToDoList = () => {
+  const [modal, setModal] = useState(false);
+  // this function toggle will set the setModal to the opposite modal state
+  const toggle = () => setModal(!modal);
   return (
-    <div>
-      
+    <div className="ToDoList">
+      <div className="div-1 text-center">
+        <h3>Todo List</h3>
+        <button className="btn btn-primary  mt-3" onClick={()=> setModal(true)} >Create Task</button>
+      </div>
+      <div className="div-2 task-container">{/* here we have the cards */}</div>
+      <CreateTask modal={modal} toggle={toggle} />
     </div>
-  )
-}
+  );
+};
 
-export default ToDoList
+export default ToDoList;
