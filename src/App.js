441e80496401683478fc6e3a086530f5a4dae3f1
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-09-28 02:04:33 +0530

    :fire: learning redux while creating the to do list app with hitesh chaudhary sir

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import ToDoList from './components/ToDoList/ToDoList';
-// due to the below line bootstrap is available to all the components in the app
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
-      <ToDoList />
+      <h1>Hi this is jatin</h1>
     </div>
   );
 }
